b3a5050c45c01288d9f744586519e9a2226fb456
sakda homhuan <sakda.homhuan@gmail.com>
2017-06-02 08:58:11 +0700

    add web
    add web

--- ng_controller.js
+++ ng_controller.js
@@ -1,13 +1,31 @@
-angular.module('app.controller', ['ui-leaflet'])
+angular.module('app.controller', ['ui-leaflet', 'ui.bootstrap'])
 
-    .controller('dashboardCtrl', function ($scope) {
+    .controller('dashboardCtrl', function ($scope, rainService) {
 
-        angular.extend($scope, {
-            center: {
+        // map
+
+        $scope.center = rainService.selectedLocation;
+
+
+        if ($scope.center.lat == null) {
+            //console.log('yess null');
+            var center = {
                 lat: 17.700,
                 lng: 100.560,
                 zoom: 9
-            },
+            }
+        } else {
+            var center = {
+                lat: Number($scope.center.lat),
+                lng: Number($scope.center.lng),
+                zoom: 15
+            };
+            //console.log($scope.center.lat + '-' + $scope.center.lng);
+        }
+
+
+        angular.extend($scope, {
+            center: center,
             markers: {
                 taipei: {
                     lat: 25.0391667,
@@ -43,8 +61,20 @@ angular.module('app.controller', ['ui-leaflet'])
                         visible: false
                     }
                 },
-                overlays: {                  
+                overlays: {
 
+                    rainnow: {
+                        name: 'ตำแหน่งวัดปริมาณน้ำฝนรายวัน',
+                        type: 'wms',
+                        visible: true,
+                        url: 'http://cgi.uru.ac.th/gs-rain/ows?',
+                        layerParams: {
+                            layers: 'rain:rain_now_report_ud',
+                            format: 'image/png',
+                            transparent: true,
+                            zIndex: 2
+                        }
+                    },
                     rainwms: {
                         name: 'ปริมาณน้ำฝนรายวัน',
                         type: 'wms',
@@ -109,5 +139,58 @@ angular.module('app.controller', ['ui-leaflet'])
                 }
             }
         });
+
+        // load rain avg
+        $scope.getStatAvg = function (avg) {
+            rainService.getStat(avg)
+                .then(function (response) {
+                    $scope.rainAvg = response.data;
+                    //console.log(response.data);
+                })
+        };
+        $scope.getStatAvg('avg');
+        
+        // load rain max
+        $scope.getStatMax = function (max) {
+            rainService.getStat(max)
+                .then(function (response) {
+                    $scope.rainMax = response.data;
+                })
+        };
+        $scope.getStatMax('max');
+        
+        // load rain max
+        $scope.getRain = function () {
+            rainService.getRain()
+                .then(function (response) {
+                    $scope.rainData = response.data;
+                    $scope.length = response.data.length;
+                    //$scope.length = response.data.length;
+                    console.log($scope.length);
+                    $scope.totalItems = $scope.length;
+                    $scope.currentPage = 1;
+                    $scope.numPerPage = 10;
+                })
+        };
+        $scope.getRain();
+
+
+        $scope.paginate = function (value) {
+            var begin, end, index;
+            begin = ($scope.currentPage - 1) * $scope.numPerPage;
+            end = begin + $scope.numPerPage;
+            index = $scope.rainData.indexOf(value);
+            return (begin <= index && index < end);
+        }
+
+        $scope.reload = function () {
+            location.reload();
+        };
+        //$scope.dat = { lat: '', lng: ''};
+        $scope.goMap = function (lat, lng) {
+            $scope.dat = { lat: lat, lng: lng };
+            rainService.selectedLocation = $scope.dat; 
+            console.log(lat+'-'+lng);
+        };
 
     })
